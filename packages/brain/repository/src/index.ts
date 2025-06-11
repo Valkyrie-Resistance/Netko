3f570930b9804262008b83665f217fdeddc720ec
PejeDev <hi@pejedev.xyz>
2025-06-10 19:22:49 -0500

    ✨ feat: prisma auth migrations and setup

---
+++ packages/brain/repository/src/index.ts
@@ -0,0 +1,2 @@
+export { prisma } from './prisma/client'
+export * from '../generated/prisma'
--- packages/brain/service/src/auth/config.ts
+++ packages/brain/service/src/auth/config.ts
@@ -1,12 +1,14 @@
-// If your Prisma file is located elsewhere, you can change the path
+import { prisma } from '@chad-chat/brain-repository'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 
-const prisma = new PrismaClient()
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  emailAndPassword: {
+    enabled: false,
+  },
   socialProviders: {
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
