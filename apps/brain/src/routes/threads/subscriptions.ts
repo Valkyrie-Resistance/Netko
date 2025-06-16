d4799a65a5eae4d3a9f83d4b16ca2a5636206724
VishKuit <vishkuit@outlook.com>
2025-06-15 20:32:13 -0500

    ✨ feat: add threadsSubscriptions to manage thread updates in the brain application

---
+++ apps/brain/src/routes/threads/subscriptions.ts
@@ -0,0 +1,25 @@
+import { observable } from '@trpc/server/observable'
+import { z } from 'zod'
+import { publicProcedure, router } from '../../lib/trpc'
+
+export const threadsSubscriptions = router({
+  onThreadUpdate: publicProcedure
+    .input(z.object({ threadId: z.string() }))
+    .query(async ({ input }) => {
+      return observable<{ threadId: string; type: 'update' | 'delete' }>((emit) => {
+        const onUpdate = (data: { threadId: string; type: 'update' | 'delete' }) => {
+          if (data.threadId === input.threadId) {
+            emit.next(data)
+          }
+        }
+
+        // TODO: Subscribe to the event emitter here (ask)
+
+        return () => {
+
+          // TODO: Unsubscribe from the event emitter here (ask)
+
+        }
+      })
+    }),
+})
